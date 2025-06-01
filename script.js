0d5cc9411f20237ac6faa2c018b527c2f3fc2bdb
nikhil969rockss <ns9013604867@gmail.com>
2025-06-01 20:51:58 +0530

    Played music

--- script.js
+++ script.js
@@ -15,12 +15,44 @@ async function getMusicData() {
     console.log(error);
   }
 }
-
+let audio = new Audio();
+function playMusic(music) {
+  music = music.replaceAll(" ", "-");
+  const path = "http://127.0.0.1:5500/audio/";
+  audio.src = path + music + ".mp3";
+  console.log(audio);
+  audio.play();
+}
 async function playSong() {
   const musics = await getMusicData();
-  const audio = new Audio();
-  audio.src = musics[0];
-  // audio.play();
-  console.log(audio);
+
+  // creating each element for songs
+  musics.forEach((e) => {
+    const ul = document.querySelector(".songs");
+    ul.innerHTML += `<li>
+                <span class="song-name"
+                  ><i class="ri-music-2-fill"></i>${e
+                    .split("/audio/")[1]
+                    .replaceAll("-", " ")
+                    .replaceAll(".mp3", "")}</span
+                >
+                <span><i class="ri-play-large-fill"></i></span>
+              </li>`;
+  });
+  const musicList = Array.from(
+    document.querySelector(".songs").getElementsByTagName("li")
+  );
+  musicList.forEach((e) => {
+    e.addEventListener("click", () => playMusic(e.innerText));
+  });
 }
 playSong();
+
+/*<li>
+                <span class="song-name"
+                  ><i class="ri-music-2-fill"></i>phela phela pyaar</span
+                >
+                <span><i class="ri-play-large-fill"></i></span>
+              </li>
+
+*/
